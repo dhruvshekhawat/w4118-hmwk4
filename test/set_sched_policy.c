a563091073d27aebe5426e1aa046b466048f5ab6
Georgios Koloventzos <gk2409@columbia.edu>
2014-11-02 23:22:06 -0500

    adding test for changing policy

---
+++ test/set_sched_policy.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sched.h>
+
+int main(int argc, char *argv[] ) {
+
+	int i, error;
+	if ( argc != 2 ) {
+		printf("USAGE: set_sched_policy <pid>\n");
+		return 0;
+	}
+	i = atoi(argv[1]);
+
+	if (i < 4) {
+		 printf("set other pid than %d\n",i);
+		 return 0;
+	}
+
+	struct sched_param param;
+	param.sched_priority = 0;
+	error = sched_setscheduler(0, 6, & param);
+	if (error == -1)
+		perror("sched");
+//	printf("error: %d",error);
+	i = 1;
+	while (i < 32767) {
+		printf("%d\n",i);
+		i++;
+	}
+	return 0;
+
+}
